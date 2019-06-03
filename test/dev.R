c4a68996f9c534bf35044154f5d20b857eb1be21
Shilin Zhao <zhaoshilin@gmail.com>
2019-06-03 15:49:01 -0500

    Bugfix and updates because of updates of maftools package

---
+++ test/dev.R
@@ -0,0 +1,132 @@
+##################################################
+#install mafreport package from github
+##################################################
+#library(devtools)
+#install_github("slzhao/mafreport")
+
+##################################################
+#load the latest version bydevtools from local
+##################################################
+devtools::load_all("~/source/mafreport")
+#Or load the installed version
+#library(mafreport)
+
+
+##################################################
+#get OS type to determin if it is on local PC or ACCRE server
+##################################################
+osInfo=.Platform$OS.type
+if (osInfo=="unix") { #ACCRE
+  reportOutDir="/scratch/cqs/zhaos/temp"
+} else if (osInfo=="windows") { #local PC
+  reportOutDir="D:\\temp\\"
+} else {
+  stop(paste0("Unknown OS type: ",osInfo))
+}
+
+
+
+##################################################
+#Prepare. Four examples. Only need one of them
+##################################################
+
+#Simple Example. TCGA data from maftools package
+mafFile=system.file('extdata', 'tcga_laml.maf.gz', package = 'maftools')
+clinicalData=system.file('extdata', 'tcga_laml_annot.tsv', package = 'maftools')
+clinicalFeatures="FAB_classification"
+#reportTemplate="d:/source/mafreport/inst/templates/report.Rmd" for test purpose
+dataForReport=initialize_maf_report_parameter(mafFile,reportOutDir=reportOutDir,
+                                               vafCol="i_TumorVAF_WU",
+                                               clinicalData=clinicalData,
+                                               clinicalFeatures=clinicalFeatures,
+                                               reportModules=c("Initialize.Rmd"))
+
+#Example for GATK. Tim's Exome sequence project
+mafFile="/gpfs23/scratch/h_vangard_1/daix4/Test_Bai/exome/result_exome/bwa_refine_gatk4_hc_gvcf_vqsr_filterMAF_annovar_filter_toMAF_MAFtools/pbs/exon.freq0.001.filtered.tsv.maf.txt"
+clinicalData="/gpfs23/scratch/h_vangard_1/daix4/Test_Bai/exome/result_exome/bwa_refine_gatk4_hc_gvcf_vqsr_filterMAF_annovar_filter_toMAF_MAFtools/pbs/family.info"
+clinicalFeatures="Family"
+dataForReport=initialize_maf_report_parameter(mafFile,reportOutDir=reportOutDir,
+                                               vc_nonSyn=c("Frame_Shift_Del", "Frame_Shift_Ins", "Splice_Site", "Translation_Start_Site",
+                                                           "Nonsense_Mutation",	"Nonstop_Mutation", "Missense_Mutation"),
+                                               interestedGenes=c("TERT","RTEL1","DKC1","PARN"),
+                                               clinicalData=clinicalData,
+                                               clinicalFeatures=clinicalFeatures
+                                               #		reportModules=c("Initialize.Rmd","SummaryTables.Rmd","GroupSummary.Rmd")
+)
+
+
+#Example for GATK. Tiger's SNV project
+mafFile="D:\\OneDriveWork\\OneDriveVanderbilt\\work\\software_QC\\test\\linton_exomeseq_2118.maf_filtered.vcf.maf.subset.maf"
+clinicalData="D:\\OneDriveWork\\OneDriveVanderbilt\\work\\software_QC\\test\\linton_exomeseq_2118.pass.family.txt"
+clinicalFeatures="family"
+dataForReport=initialize_maf_report_parameter(mafFile,reportOutDir=reportOutDir,
+                                               vc_nonSyn=c("Frame_Shift_Del", "Frame_Shift_Ins", "Splice_Site", "Translation_Start_Site",
+                                                           "Nonsense_Mutation",	"Nonstop_Mutation", "Missense_Mutation"),
+                                               interestedGenes=c("LDLR","APOB","PCSK9","LDLRAP1","STAP1","LIPA","ABCG5","ABCGB","APOE","LPA","PNPLA5","CH25H","INSIG2","SIRT1"),
+                                               clinicalData=clinicalData,
+                                               clinicalFeatures=clinicalFeatures
+                                               #		reportModules=c("Initialize.Rmd","SummaryTables.Rmd","GroupSummary.Rmd")
+)
+
+#Example for GATK. Tiger's SNV project in ACCRE
+mafFile="/scratch/cqs/shengq2/macrae_linton/20190517_linton_exomeseq_3321_human/bwa_refine_gatk4_hc_gvcf_vqsr_filterMAF_annovar_filter_toMAF/result/linton_exomeseq_3321.freq0.001.filtered.tsv.maf"
+clinicalData="/scratch/cqs/shengq2/macrae_linton/20180913_linton_exomeseq_2118_human_cutadapt/linton_exomeseq_2118.pass.family.txt"
+clinicalFeatures="family"
+interestedGeneStr="LDLR APOB PCSK9 LDLRAP1 STAP1 LIPA ABCG5 ABCGB APOE LPA PNPLA5 CH25H INSIG2 SIRT1"
+if(!is.null(interestedGeneStr)) {
+  interestedGeneStr = gsub("\\s+", ",", interestedGeneStr)
+  interestedGenes = unlist(strsplit(interestedGeneStr, ","))
+}else{
+  interestedGenes = NULL
+}
+dataForReport=initialize_maf_report_parameter(mafFile,reportOutDir=reportOutDir,
+                                              vc_nonSyn=c("Frame_Shift_Del", "Frame_Shift_Ins", "Splice_Site", "Translation_Start_Site",
+                                                          "Nonsense_Mutation",	"Nonstop_Mutation", "Missense_Mutation"),
+                                              interestedGenes=interestedGenes
+#                                              clinicalData=clinicalData,
+#                                              clinicalFeatures=clinicalFeatures,
+#                                              reportModules=c("Initialize.Rmd","SummaryTables.Rmd","GroupSummary.Rmd")
+)
+
+#Example for mouse. Jing's WES somatic mutation project
+mafFile="/data/CRC_expression/JingYang/result/2221/bwa_refine_muTect_combined_annovar_toMAF/result/2221_mm10_pass.combined.vcf.annovar.final.tsv.maf"
+dataForReport=initialize_maf_report_parameter(mafFile,reportOutDir=reportOutDir,
+                                               genome="mm10",dndscv.refdb="/scratch/cqs/references/dndscv/RefCDS_mouse_GRCm38.p2.rda")
+
+##Example for MuTect2 and hg38. Pierre's WES project
+mafFile="D:\\OneDriveWork\\OneDriveVanderbilt\\work\\Pierre\\20180830_WESTestResult\\20181206_LungWesVcfToMaf.all.subset.maf"
+dataForReport=initialize_maf_report_parameter(mafFile,reportOutDir=reportOutDir,genome="hg38",
+                                               #		reportModules=c("Initialize.Rmd","HighFreqVariantsCheck.Rmd"),
+                                               dndscv.refdb="D:\\OneDriveWork\\OneDriveVanderbilt\\work\\software_QC\\test\\RefCDS_human_GRCh38.p12.rda",
+                                               clinicalData="D:\\OneDriveWork\\OneDriveVanderbilt\\work\\Pierre\\20180830_WESTestResult\\wes_sample_grp.txt",
+                                               clinicalFeatures=c("PatientID","SampleGroup")
+                                               #		reportTemplate="d:/source/mafreport/inst/templates/report.Rmd"
+)
+
+##Example for JarVarDict and hg38. Pierre's WES project
+mafFile="/gpfs23/scratch/cqs/zhaos/Pierre/WES/20190509_VarDict_vcf2maf/20190522_LungWesUmiVcfToMaf.all.subset.maf"
+reportOutDir="/gpfs23/scratch/cqs/zhaos/Pierre/WES/20190509_VarDict_vcf2maf/"
+dataForReport=initialize_maf_report_parameter(mafFile,reportOutDir=reportOutDir,genome="hg38",
+                                               #reportModules=c("Initialize.Rmd","SummaryTables.Rmd","VariantsVisualization.Rmd","HighFreqVariantsCheck.Rmd"),
+                                               dndscv.refdb="/scratch/cqs/references/dndscv/RefCDS_mouse_GRCm38.p2.rda"
+                                               #                                               clinicalData="/gpfs23/scratch/cqs/zhaos/Pierre/WES/wes_sample_grp.txt",
+                                               #                                               clinicalFeatures=c("PatientID","SampleGroup")
+                                               #		reportTemplate="d:/source/mafreport/inst/templates/report.Rmd"
+)
+
+##################################################
+#Make report
+##################################################
+dataForReport=make_maf_report(dataForReport)
+
+
+##################################################
+#Result
+##################################################
+#Html Report: *.maf.report.html
+#RData to reproduce Report: *.maf.report.html.RData
+
+
+
+
+
